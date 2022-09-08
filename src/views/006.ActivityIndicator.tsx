07f0206696432e81bf84aad7feec754538716b30
Dendi <352671309@qq.com>
2022-09-08 23:06:44 +0800

    Feat: ActivityIndicator 基本使用

--- src/views/006.ActivityIndicator.tsx
+++ src/views/006.ActivityIndicator.tsx
@@ -1,20 +1,27 @@
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, View, Dimensions, ActivityIndicator} from 'react-native'
 import React from 'react'
 
 const Demo06 = () => {
   return (
     <View style={styl.main}>
-      <Text>Demo06</Text>
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" color="#0000ff" />
+      <ActivityIndicator size="large" color="#00ff00" />
     </View>
   )
 }
 
 export default Demo06
 
+const vw = Dimensions.get('window').width - 10
 const styl = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
+    marginHorizontal: 5,
+    width: vw,
+    backgroundColor: 'skyblue',
   },
 })
