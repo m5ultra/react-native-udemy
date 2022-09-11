7becf0f27895177d69006f33b0b3e7ec24f81bd7
Dendi <352671309@qq.com>
2022-09-11 14:29:41 +0800

    Feat: Test AsyncStorage Use

--- src/views/17.asyncStorage.tsx
+++ src/views/17.asyncStorage.tsx
@@ -1,19 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
+import {View, Text, Button} from 'react-native'
+import {useAsyncStorage} from '@react-native-async-storage/async-storage'
 
-const Demo17 = () => {
+export default function Demo17() {
+  const [value, setValue] = useState('value')
+  const {getItem, setItem} = useAsyncStorage('@storage_key')
+
+  const readItemFromStorage = useCallback(async () => {
+    const item = await getItem()
+    setValue(item as string)
+  }, [getItem])
+
+  const writeItemToStorage = async (newValue: string) => {
+    console.log(newValue, 'newVal')
+    await setItem(newValue)
+    setValue(newValue)
+  }
+
+  useEffect(() => {
+    readItemFromStorage()
+  }, [readItemFromStorage])
+
   return (
-    <View style={styl.main}>
-      <Text>Demo17</Text>
+    <View style={{margin: 40}}>
+      <Text>Current value: {value}</Text>
+      <Button
+        onPress={() =>
+          writeItemToStorage(Math.random().toString(36).substr(2, 5))
+        }
+        title={'Update Value'}
+      />
     </View>
   )
 }
-
-export default Demo17
-
-const styl = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: 'green',
-  },
-})
