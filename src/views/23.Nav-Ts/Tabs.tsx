ba088acbc6ca41399a0f9100b9b01bfc6a747822
Dendi <352671309@qq.com>
2022-10-01 20:25:45 +0800

    Feat: 底部导航器设置字体和颜色

--- src/views/23.Nav-Ts/Tabs.tsx
+++ src/views/23.Nav-Ts/Tabs.tsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
+import Icon from 'react-native-vector-icons/Ionicons'
+import {IconSettings} from '../../icons'
 import {BottomTabNavigatorParamList} from './types'
 import HomeStackNavigator from './HomeStack'
 import FeedScreen from './FeedScreen'
 import SettingsScreen from './SettingsScreen'
 
-const Tab = createBottomTabNavigator<BottomTabNavigatorParamList>()
+const BottomTab = createBottomTabNavigator<BottomTabNavigatorParamList>()
 
 const BottomTabs = () => {
+  const activeColor = '#E4393C'
   return (
-    <Tab.Navigator>
-      <Tab.Screen
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: activeColor,
+        tabBarInactiveTintColor: 'gray',
+      }}>
+      <BottomTab.Screen
         name="HomeStack"
         component={HomeStackNavigator}
-        options={{headerShown: false}}
+        options={{
+          title: '首页',
+          headerShown: false,
+          tabBarIcon: ({focused, size}) => {
+            const focusedColor = focused ? activeColor : 'gray'
+            return <Icon name={'home'} size={size} color={focusedColor} />
+          },
+        }}
       />
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
+      <BottomTab.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={{
+          title: '苹果',
+          tabBarIcon: ({focused, size}) => {
+            const focusedColor = focused ? activeColor : 'gray'
+            return <Icon name={'logo-apple'} size={size} color={focusedColor} />
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: '设置',
+          tabBarIcon: ({focused}) => {
+            const focusedColor = focused ? activeColor : 'gray'
+            return <IconSettings size={20} color={focusedColor} />
+          },
+        }}
+      />
+    </BottomTab.Navigator>
   )
 }
 
